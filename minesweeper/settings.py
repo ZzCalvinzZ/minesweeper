c42544f8fe8edc3ac28237f4ca7ddb1a13d4bb19
Calvin <calvinkcollins@gmail.com>
2014-10-20 19:48:57 -0400

    updates for heroku

--- minesweeper/settings.py
+++ minesweeper/settings.py
@@ -4,6 +4,16 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -115,7 +125,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/calvin/dev/minesweeper/templates/'
+    '/home/calvin/dev/minesweeper/templates/',
+    root('templates'),
 )
 
 INSTALLED_APPS = (
