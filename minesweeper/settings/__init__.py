3882d21339f5f7f34d13371425293d76668d241d
Calvin Collins <calvinc@surveymonkey.com>
2016-01-06 12:16:50 -0500

    fix template_dirs

--- minesweeper/settings/__init__.py
+++ minesweeper/settings/__init__.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # holding the current file.
 here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
-PROJECT_ROOT = here("..")
+PROJECT_ROOT = here("../..")
 # root() gives us file paths from the root of the system to whatever
 # folder(s) we pass it starting at the parent directory of the current file.
 root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
