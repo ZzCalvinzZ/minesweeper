5419c2f56f17a3dda930719972d896cadaa073b4
Calvin Collins <calvinc@surveymonkey.com>
2016-01-07 10:36:54 -0500

    fix name entry validation

--- game/forms.py
+++ game/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.validators import RegexValidator
 
 class GameForm(forms.Form):
     DIFFICULTY = (
@@ -6,5 +7,7 @@ class GameForm(forms.Form):
       ('intermediate','intermediate'),
       ('expert','expert'),
       )
-    name = forms.CharField(label='Name', max_length=100)
+
+    alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
+    name = forms.CharField(label='Name', max_length=100, validators=[alphanumeric])
     difficulty = forms.ChoiceField(choices=DIFFICULTY, required=True)
